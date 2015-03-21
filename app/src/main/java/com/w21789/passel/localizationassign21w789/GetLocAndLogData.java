0c8ba8b23ffe69c64cc38dca00dd1f065de2da47
Lisandro Jimenez <jimenezl@mit.edu>
2015-03-20 22:03:57 -0400

    App with background service that writes to logfile and main activity
    The background service writes to a log file every ten seconds with a
    message, and also updates the main activity with this message

---
+++ app/src/main/java/com/w21789/passel/localizationassign21w789/GetLocAndLogData.java
@@ -0,0 +1,89 @@
+package com.w21789.passel.localizationassign21w789;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Environment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GetLocAndLogData extends Service {
+    //For running automated tasks
+    private Timer timer;
+
+    public GetLocAndLogData() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    //write to log file
+    public void appendLog(String text) {
+        File externalStorageDir = Environment.getExternalStorageDirectory();
+        File logFile = new File(externalStorageDir, "localization_log.txt");
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            //BufferedWriter for performance, true to set append to file flag
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(text);
+            buf.newLine();
+            buf.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    private void sendMessage(String message) {
+        Log.d("sender", "Broadcasting message");
+        Intent intent = new Intent("send-localization-data");
+        // You can also include some extra data.
+        intent.putExtra("message", message);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+
+        int delay = 1000; // delay for 1 sec.
+        int period = 10000; // repeat every 10 sec.
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+            public void run()
+            {
+                String message = "Hello!";
+                sendMessage(message);  // send to the main Activity so user can see
+                appendLog(message);
+            }
+        }, delay, period);
+
+        return START_NOT_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/w21789/passel/localizationassign21w789/MainActivity.java
+++ app/src/main/java/com/w21789/passel/localizationassign21w789/MainActivity.java
@@ -1,19 +1,59 @@
 package com.w21789.passel.localizationassign21w789;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ArrayList<String> dataItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this here is needed so we can update the ListView as we get data
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                dataItems);
+        ListView dataListView = (ListView)findViewById(R.id.dataListView);
+        dataListView.setAdapter(adapter);
+
+        //Local messages passed go through here
+        LocalBroadcastManager.getInstance(this).registerReceiver(localizationMessageReceiver,
+                new IntentFilter("send-localization-data"));
     }
 
+    private BroadcastReceiver localizationMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            String message = intent.getStringExtra("message");
+            addItems(message);
+            Log.d("receiver", "Got message: " + message);
+        }
+    };
+
+    //Adds a new row to the ListView with contents of message
+    public void addItems(String message) {
+        dataItems.add(message);
+        adapter.notifyDataSetChanged();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +75,21 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Start gathering data!
+    public void startGathering(View view) {
+        startService(new Intent(getBaseContext(), GetLocAndLogData.class));
+    }
+
+    //Stop gathering data!
+    public void stopGathering(View view) {
+        stopService(new Intent(getBaseContext(), GetLocAndLogData.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(localizationMessageReceiver);
+        super.onDestroy();
     }
 }
